ecbe4c9214b34871bb0c6294cfd827b2c0c20604
sahar vatankhah <Svatankhah50@gmail.com>
2024-06-27 03:53:15 -0700

    add workshop code

---
+++ script.js
@@ -0,0 +1,9 @@
+const search = document.querySelector("#search");
+const submit = document.querySelector("#submit");
+const random = document.querySelector("#random");
+const mealsElement = document.querySelector("#meals");
+const resultHeading = document.querySelector("#result-heading");
+const singleMealElement = document.querySelector("#single-meal");
+
+
+//// https://www.themealdb.com/api/json/v1/1/search.php?s=${user}
